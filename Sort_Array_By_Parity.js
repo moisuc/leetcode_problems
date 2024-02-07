488959e93c05a30727b7496ca4fc0e896823a53f
moisuc <moisuc.catalin@gmail.com>
2024-02-07 10:02:42 +0200

    Create Sort_Array_By_Parity.js

---
+++ Sort_Array_By_Parity.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortArrayByParity = function(nums) {
+    let j=0;
+    let temp = 0;
+    for(let i = 0; i<nums.length;i++){
+        if(nums[i]%2==0){
+            temp = nums[j];
+            nums[j]=nums[i];
+            nums[i]=temp;
+            j++;
+        }
+    }
+    return nums;
+};
